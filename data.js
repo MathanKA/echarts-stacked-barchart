286dc47d903caa43f898bfcff520d69811e1c2fc
Mathan K A <mathanka@live.in>
2020-04-16 06:30:40 +0000

    Initial commit

---
+++ data.js
@@ -0,0 +1,137 @@
+export const DATA = [
+  {
+    shop: 'exma shop',
+    vegs: [
+      {
+        branch: 'area 1',
+        count: 15
+      },
+      {
+        branch: 'area 2',
+        count: 2
+      },
+            {
+        branch: 'area 3',
+        count: 21
+      },
+    ],
+    fruits: [
+      {
+        branch: 'area 1',
+        count: 151
+      },
+      {
+        branch: 'area 2',
+        count: 23
+      },
+            {
+        branch: 'area 3',
+        count: 34
+      },
+    ],
+    nonVeg: [
+      {
+        branch: 'area 1',
+        count: 25
+      },
+      {
+        branch: 'area 2',
+        count: 29
+      },
+            {
+        branch: 'area 3',
+        count: 22
+      },
+    ]
+  },
+  {
+    shop: 'Metro',
+    vegs: [
+      {
+        branch: 'area 1',
+        count: 31
+      },
+      {
+        branch: 'area 2',
+        count: 54
+      },
+            {
+        branch: 'area 3',
+        count: 65
+      },
+    ],
+    fruits: [
+      {
+        branch: 'area 1',
+        count: 75
+      },
+      {
+        branch: 'area 2',
+        count: 43
+      },
+            {
+        branch: 'area 3',
+        count: 62
+      },
+    ],
+    nonVeg: [
+      {
+        branch: 'area 1',
+        count: 12
+      },
+      {
+        branch: 'area 2',
+        count: 43
+      },
+            {
+        branch: 'area 3',
+        count: 87
+      },
+    ]
+  },
+  {
+    shop: 'ABC',
+    vegs: [
+      {
+        branch: 'area 1',
+        count: 04
+      },
+      {
+        branch: 'area 2',
+        count: 34
+      },
+            {
+        branch: 'area 3',
+        count: 88
+      },
+    ],
+    fruits: [
+      {
+        branch: 'area 1',
+        count: 82
+      },
+      {
+        branch: 'area 2',
+        count: 69
+      },
+            {
+        branch: 'area 3',
+        count: 99
+      },
+    ],
+    nonVeg: [
+      {
+        branch: 'area 1',
+        count: 19
+      },
+      {
+        branch: 'area 2',
+        count: 47
+      },
+            {
+        branch: 'area 3',
+        count: 90
+      },
+    ]
+  }
+]
